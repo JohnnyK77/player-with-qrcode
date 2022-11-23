04635a161c7f5bbf52182b75b77896f1a27fe33d
JohnnyK77 <max7up@naver.com>
2022-11-23 20:11:29 +0900

    feat: player view 추가
    - exoplayer
    - INTERNET 사용 권한 추가 : video url 재생

--- player/app/src/main/java/com/example/qradvertiseplayer/MainActivity.kt
+++ player/app/src/main/java/com/example/qradvertiseplayer/MainActivity.kt
@@ -2,17 +2,30 @@ package com.example.qradvertiseplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.qradvertiseplayer.databinding.ActivityMainBinding
+import com.google.android.exoplayer2.Player
 import com.google.zxing.BarcodeFormat
 import com.journeyapps.barcodescanner.BarcodeEncoder
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Player.Listener {
+    private val TAG = javaClass.simpleName.trim()
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        setPlayer()
+    }
+
+    private fun setPlayer() {
+        binding.epvMain.init(this)
+    }
+
+    private fun startPlay(url: String) {
+        binding.epvMain.setPlayMediaItem(url)
+        binding.epvMain.start()
     }
 
     private fun setQrCodeImage(content: String) {
@@ -24,5 +37,17 @@ class MainActivity : AppCompatActivity() {
             binding.ivQrCode.layoutParams.height
         )
         binding.ivQrCode.setImageBitmap(bitmap)
+    }
+
+    override fun onPlaybackStateChanged(state: Int) {
+        Log.d(TAG, "state : $state")
+        when (state) {
+            Player.STATE_ENDED -> {
+                Log.d(TAG, "end")
+
+            }
+            Player.STATE_READY -> {}
+            Player.STATE_BUFFERING -> {}
+        }
     }
 }
---
+++ player/app/src/main/java/com/example/qradvertiseplayer/player/ExoPlayerView.kt
@@ -0,0 +1,81 @@
+package com.example.qradvertiseplayer.player
+
+import android.content.Context
+import android.graphics.Color
+import android.net.Uri
+import android.util.AttributeSet
+import android.util.Log
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.ui.StyledPlayerView
+
+class ExoPlayerView : StyledPlayerView {
+    private val TAG = javaClass.simpleName.trim()
+    private var mPlayer: ExoPlayer? = null
+    private var mListener: Player.Listener? = null
+
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
+
+    fun init(listener: Player.Listener) {
+        mListener = listener
+        //영상만 나오게 player 설정
+        setShutterBackgroundColor(Color.TRANSPARENT)
+        useController = false
+        mPlayer = ExoPlayer.Builder(context).build()
+        this.player = mPlayer
+        if (mListener != null) {
+            mPlayer!!.addListener(mListener!!)
+        }
+    }
+
+    fun setPlayMediaItem(url: String) {
+        if (mPlayer != null) {
+            val mediaItem = MediaItem.fromUri(url)
+            mPlayer!!.setMediaItem(mediaItem)
+            mPlayer!!.prepare()
+        }
+    }
+
+    fun isPlaying(): Boolean {
+        return mPlayer?.isPlaying == true
+    }
+
+    fun pause() {
+        if (isPlaying()) {
+            Log.d(TAG, "pause called")
+            mPlayer?.pause()
+        }
+    }
+
+    fun start() {
+        if (!isPlaying()) {
+            Log.d(TAG, "start called")
+            mPlayer?.play()
+        }
+    }
+
+    //동영상 시간 설정
+    fun seekTo(position: Int) {
+        mPlayer?.seekTo(position.toLong())
+    }
+
+    //동영상 해제
+    fun releasePlayer() {
+        Log.d(TAG, "releasePlayer called!")
+        //            mPlayer.stop();
+        mPlayer?.release()
+        mPlayer = null
+    }
+
+    fun seekToEnd() {
+        mPlayer?.seekTo(mPlayer!!.duration)
+//        mPlayer.stop(true);
+    }
+}
