97f67a1dd4e60fbb04d3020a05d3bf9a47458d47
kimjh05 <max7up@naver.com>
2022-11-23 16:57:39 +0900

    feat: qr코드 generator 구현

--- app/src/main/java/com/example/qradvertiseplayer/MainActivity.kt
+++ app/src/main/java/com/example/qradvertiseplayer/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.qradvertiseplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.qradvertiseplayer.databinding.ActivityMainBinding
+import com.google.zxing.BarcodeFormat
+import com.journeyapps.barcodescanner.BarcodeEncoder
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
+    private fun setQrCodeImage(content: String) {
+        val barcodeEncoder = BarcodeEncoder()
+        val bitmap = barcodeEncoder.encodeBitmap(
+            content,
+            BarcodeFormat.QR_CODE,
+            binding.ivQrCode.layoutParams.width,
+            binding.ivQrCode.layoutParams.height
+        )
+        binding.ivQrCode.setImageBitmap(bitmap)
     }
 }
