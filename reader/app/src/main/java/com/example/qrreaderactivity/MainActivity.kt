70c0fb106ed57c19c6f8f2f4f18c223998bc7dec
kimjh05 <118873509+JohnnyK77@users.noreply.github.com>
2022-11-25 18:22:22 +0900

    feat: add qr read page

--- reader/app/src/main/java/com/example/qrreaderactivity/MainActivity.kt
+++ reader/app/src/main/java/com/example/qrreaderactivity/MainActivity.kt
@@ -1,11 +1,54 @@
 package com.example.qrreaderactivity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.qrreaderactivity.databinding.ActivityMainBinding
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanIntentResult
+import com.journeyapps.barcodescanner.ScanOptions
+
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = javaClass.simpleName.trim()
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        init()
     }
+
+    private fun init() {
+        //set scanner options
+        val options = ScanOptions()
+        options.setDesiredBarcodeFormats(ScanOptions.QR_CODE)
+        options.setPrompt("Scan a qrcode")
+        options.setCameraId(0) // Use a specific camera of the device
+        options.setBeepEnabled(true)
+        options.setBarcodeImageEnabled(true)
+
+        binding.btnStartScan.setOnClickListener {
+            barcodeLauncher.launch(options) //start scanner activity
+        }
+    }
+
+    private fun sendUserInfo() {
+
+    }
+
+    // Registet scanner activity
+    private val barcodeLauncher = registerForActivityResult(
+        ScanContract()
+    ) { result: ScanIntentResult ->
+        if (result.contents == null) {
+            Toast.makeText(this@MainActivity, "Cancelled", Toast.LENGTH_LONG).show()
+        } else {
+//            Toast.makeText(this@MainActivity, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
+            binding.tvResult.text = result.contents
+        }
+    }
+
 }
